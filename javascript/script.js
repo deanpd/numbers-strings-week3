46ff96dbafcd3b335c73814068f6b83f510c184d
deanpd <dean@deanpd.com>
2016-06-02 19:06:11 -0700

    Check for NaN.

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,19 @@
 $("document").ready(function(){
 
+checkInput();
   
-  
+function checkInput(){
+  var inputText = $('#input-text').val();
+  if (isNaN(inputText)){
+    alert("That is not a number.");
+    return false;
+  }
+}
 });
+
+//function checkInput(){
+//  var inputText = $('#input-text').val();
+//  if (isNaN(inputText.value) == true){
+//    alert("That is not a number.");
+//  }
+//}
